d2b5fbdd41b327020e45d1bdd0db074377f07ee9
小村奈央 <nao0117.08194@icloud.com>
2026-07-02 06:41:05 +0000

    ログアウト機能を作成しよう

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const knex = require('../db/knex');
 router.get('/', function (req, res, next) {
   const userId = req.session.userid;
   const isAuth = Boolean(userId);
-  console.log(`isAuth: ${isAuth}`);
 
   knex("tasks")
     .select("*")
@@ -40,5 +39,6 @@ router.post('/', function (req, res, next) {
 
 router.use('/signup', require('./signup'));
 router.use('/signin', require('./signin'));
+router.use('/logout', require('./logout'));
 
 module.exports = router;
